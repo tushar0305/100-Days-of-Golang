06fb3e666caa4fff3289b6ec0f7b29f0dd8119b4
Tushar Kasaudhan <tusharkasaudhan6@gmail.com>
2024-03-07 23:18:35 +0530

    3rd Code of day4

--- Day4/main.go
+++ Day4/main.go
@@ -1,25 +1,23 @@
-// // Golang Functions Returning Multiple Values
-// package main
+// Golang Functions Returning Multiple Values
+package main
 
-// import "fmt"
+import "fmt"
 
-// func rectangle(l int, b int) (area int, para int) {
-// 	area = l * b
-// 	para = 2 * (l + b)
-// 	return
-// }
+func rectangle(l int, b int) (area int, para int) {
+	area = l * b
+	para = 2 * (l + b)
+	return
+}
 
-// func main() {
-// 	var l int
-// 	var b int
-// 	fmt.Print("Enter the length and breadth of the rectangle: ")
-// 	fmt.Scan(&l, &b)
-// 	a, p := rectangle(l, b)
-// 	fmt.Println("The area of the rectangle is:", a)
-// 	fmt.Println("The perimeter of the rectangle is:", p)
-// }
-
-
+func main() {
+	var l int
+	var b int
+	fmt.Print("Enter the length and breadth of the rectangle: ")
+	fmt.Scan(&l, &b)
+	a, p := rectangle(l, b)
+	fmt.Println("The area of the rectangle is:", a)
+	fmt.Println("The perimeter of the rectangle is:", p)
+}
 
 // Golang Passing Address to a Function
 package main
@@ -42,4 +40,26 @@ func main() {
 	update(&a, &b)
 
 	fmt.Println("After : ", a, b)
+}
+
+// GO Program to Swap Number Without Using Temporary Variables
+
+package main
+
+import "fmt"
+
+func main() {
+	var a int
+	var b int
+	fmt.Printf("Enter the first number: ")
+	fmt.Scanln(&a)
+
+	fmt.Printf("Enter the second number: ")
+	fmt.Scanln(&b)
+
+	a = a - b    
+	b = a + b        
+	a = b - a
+	fmt.Printf("After Swapping: %d %d\n", a, b)
+
 }
