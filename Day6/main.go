972b7b59042ac11e1c788bf92a547d2324ea8f6b
tushar0305 <tusharkasaudhan6@gmail.com>
2024-03-09 23:51:32 +0530

    Day6 ssecond program

--- Day6/main.go
+++ Day6/main.go
@@ -1,6 +1,6 @@
 // Initialize Slice with values using a Slice Literal
 
-package main
+/*package main
 
 import "fmt"
 
@@ -10,5 +10,20 @@ func main(){
 
 	fmt.Printf("intSlice \tLen: %v \tCap: %v\n", len(intSlice), cap(intSlice))
 	fmt.Printf("strSlice \tLen: %v \tCap: %v\n", len(strSlice), cap(strSlice))
-}
+}*/
 
+package main
+
+import "fmt"
+
+func main() {
+	a := make([]int, 2, 5)
+	a[0] = 10
+	a[1] = 20
+	fmt.Println("Slice A:", a)
+	fmt.Printf("Length is %d Capacity is %d\n", len(a), cap(a))
+
+	a = append(a, 30, 40, 50, 60, 70, 80, 90)
+	fmt.Println("Slice A after appending data:", a)
+	fmt.Printf("Length is %d Capacity is %d\n", len(a), cap(a))
+}
